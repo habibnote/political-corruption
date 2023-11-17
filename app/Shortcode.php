0fc7e7a0a3e94c9ea06be6087bc72ccaa820d7fa
Habibur Rahman <habibblog97@gmail.com>
2023-11-17 12:54:23 +0600

    user login shortcode added

--- app/Shortcode.php
+++ app/Shortcode.php
@@ -12,13 +12,22 @@ class Shortcode {
      */
     function __construct() {
         add_shortcode( 'pc_register', [$this, 'pc_user_registration'] );
+        add_shortcode( 'pc_login', [$this, 'pc_user_login'] );
     }
 
     /**
-     * User ragistration
+     * User Login
+    */
+    function pc_user_login() {
+
+        include_once( PC_DIR . "/view/form/user-login.php" );
+    }
+
+    /**
+     * User Ragistration
      */
     function pc_user_registration() {
 
-        include_once( PC_DIR . "/view/form/user-ragistration-form.php" );
+        include_once( PC_DIR . "/view/form/user-ragistration.php" );
     }
 }
